6ca165e107ed16340d2d722ad10d013e74489078
codingpanda23 <league@WorthyMini-5.attlocal.net>
2017-06-10 17:26:12 -0700

    fgbh

--- src/RandomStuff.java
+++ src/RandomStuff.java
@@ -1,10 +1,8 @@
+import javax.swing.JOptionPane;
 
 public class RandomStuff {
-	static int i = 0;
-public static void main(String[] args) {
-	
-}
-static void Test(){
-	i = 10;
-}
+
+	public static void main(String[] args) {
+		
+	}
 }
